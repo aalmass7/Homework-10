49e7cfb5d5cac82b2d44f2592b0e1f4a1895fc02
Almas <aalmasabilov@gmail.com>
2026-05-08 23:11:11 +0500

    Implement scout message handling

--- src/com/narxoz/rpg/guild/Scout.java
+++ src/com/narxoz/rpg/guild/Scout.java
@@ -10,12 +10,25 @@ public class Scout extends GuildMember {
     }
 
     public void reportRoute(String topic, String payload) {
-        // TODO: send a scouting message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+
+        if (GuildHall.ORDERS.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] received order from " + sender
+                    + ": checking the safest road. Details: " + payload);
+        } else if (GuildHall.SCOUTING.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] received scouting topic from " + sender
+                    + ": updating the route map. Details: " + payload);
+        } else if (GuildHall.LORE.equals(topic)) {
+            System.out.println("[Scout " + getName() + "] received lore from " + sender
+                    + ": marking dangerous ruins on the map. Details: " + payload);
+        } else {
+            System.out.println("[Scout " + getName() + "] received " + topic
+                    + " from " + sender + ": " + payload);
+        }
     }
 }
