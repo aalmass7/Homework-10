df270cdf90fd7ba3655cec6ee192995f3536534f
Almas <aalmasabilov@gmail.com>
2026-05-08 23:02:14 +0500

    Implement quartermaster message handling

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -10,12 +10,25 @@ public class Quartermaster extends GuildMember {
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+
+        if (GuildHall.ORDERS.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] received order from "
+                    + sender + ": preparing standard gear. Details: " + payload);
+        } else if (GuildHall.SUPPLIES.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] received supply request from "
+                    + sender + ": counting rations and rope. Details: " + payload);
+        } else if (GuildHall.SCOUTING.equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] received scouting update from "
+                    + sender + ": changing travel supplies. Details: " + payload);
+        } else {
+            System.out.println("[Quartermaster " + getName() + "] received " + topic
+                    + " from " + sender + ": " + payload);
+        }
     }
 }
