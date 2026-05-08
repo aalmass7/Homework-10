723a60468001852b7ba471904017b6f0fba978b7
Almas <aalmasabilov@gmail.com>
2026-05-08 23:18:40 +0500

    Implement healer message handling

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -10,12 +10,25 @@ public class Healer extends GuildMember {
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+
+        if (GuildHall.ORDERS.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] received order from " + sender
+                    + ": packing healing kits. Details: " + payload);
+        } else if (GuildHall.HEALING.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] received healing topic from " + sender
+                    + ": preparing bandages and potions. Details: " + payload);
+        } else if (GuildHall.CURSE.equals(topic)) {
+            System.out.println("[Healer " + getName() + "] received curse warning from " + sender
+                    + ": preparing cleansing herbs. Details: " + payload);
+        } else {
+            System.out.println("[Healer " + getName() + "] received " + topic
+                    + " from " + sender + ": " + payload);
+        }
     }
 }
